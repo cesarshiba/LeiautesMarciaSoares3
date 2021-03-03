dc7dd076c1e318318451e4d7bad9ac594b549f59
cesar <cesar@DESKTOP-DDJ9EOP>
2021-03-03 15:22:25 -0300

    Experiencing GridPane settings;
    Adjusted TableView at center position;
    
    Config button with circle shape;

--- LeiautesMarciaSoares3/src/main/java/br/com/cesarshiba/leiautesmarciasoares/FrmPrincipal.java
+++ LeiautesMarciaSoares3/src/main/java/br/com/cesarshiba/leiautesmarciasoares/FrmPrincipal.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.util.Duration;
@@ -110,46 +111,39 @@ public class FrmPrincipal implements Initializable{
 		/*
 		 * Nome do cliente escolhido
 		 */
-		Label label = new Label("Cliente Escolhido");
-		label.setFont(new Font("Arial Black", 45));
+		Label label = new Label("Cliente Selecionado");
+		label.setFont(new Font("Arial Black", 40));
 		label.setTextFill(Color.WHITE);
 		label.setAlignment(Pos.CENTER);
-		//label.setPrefWidth(1000);
+		label.setPrefWidth(1000);
 		/*
 		 * Painel para hospedar o label com nome do cliente
 		 */
 		Pane pane = new Pane();
-		//pane.setPrefSize(1000, 100);
 		pane.setStyle("-fx-background-radius: 20 20 20 20; -fx-border-radius: 20 20 20 20; -fx-background-color:#ffa000");
 		pane.getChildren().add(label);
 		/*
 		 * Botão de config
 		 */
-		Image imagem = new Image(MainClass.caminho() +"/config.png");
+		Image imagem = new Image(MainClass.caminho() +"/Config.png");
 		ImageView imageView = new ImageView(imagem);
 		imageView.setFitHeight(80);
 		imageView.setFitWidth(80);
 		Button botao = new Button();
 		botao.setGraphic(imageView);
-		botao.setStyle("-fx-background-radius: 20 20 20 20; -fx-border-radius: 20 20 20 20; -fx-background-color:#fddbaf" );
-		/*
-		 * Ajuste da foto no painel reduzido
-		 */
-		Image foto = new Image(MainClass.caminho() +"/Paisagem.jpg");
-		ImageView fotoView = new ImageView(foto);
-		fotoView.setFitHeight(628);
-		fotoView.setFitWidth(1000);
-		fotoView.fitHeightProperty();
-		fotoView.fitWidthProperty();
+		double r = 40;
+		botao.setShape(new Circle(r));
+		botao.setMinSize(2*r, 2*r);
+		botao.setMaxSize(2*r, 2*r);
 		/*
 		 * Montagem do grid de painéis
 		 */
 		GridPane gridPane = new GridPane();
 		gridPane.add(pane, 8, 2, 8, 2);
 		gridPane.add(botao, 16, 2, 2, 2);
-		gridPane.add(fotoView, 8, 4, 8, 8);
 		gridPane.setHgap(15);
-		//gridPane.setVgap(15);
+		gridPane.setVgap(15);
+		gridPane.setAlignment(Pos.TOP_CENTER);
 		root.setCenter(gridPane);
 		botao.setOnAction(value -> {
 			System.out.println("Apertou botão CONFIG");
@@ -167,10 +161,9 @@ public class FrmPrincipal implements Initializable{
 	 */
 	private void montaPainelClientes() {
 	    TableView<ClienteNomeSexoEmail> tblClientes = new TableView<>();
-	    tblClientes.setPrefWidth(582.0);
+	    tblClientes.setPrefWidth(600.0);
+	    tblClientes.setPrefHeight(600.0);
 	    tblClientes.setStyle("-fx-background-radius: 20 20 20 20; -fx-border-radius: 20 20 20 20;");
-	    //tblClientes.setLayoutX(9.0);
-	    //tblClientes.setLayoutY(36.0);
 	    ObservableList<ClienteNomeSexoEmail> obsClientes;
 	    TableColumn<ClienteNomeSexoEmail, String> colNomeCliente = new TableColumn<ClienteNomeSexoEmail, String>();
 	    colNomeCliente.setPrefWidth(266.0);
@@ -188,18 +181,9 @@ public class FrmPrincipal implements Initializable{
     	tblClientes.getColumns().addAll(colNomeCliente, colSexoCliente, colEmailCliente);
     	tblClientes.setItems(obsClientes);
 
-		Image foto = new Image(MainClass.caminho() +"/Paisagem.jpg"); //Ajuste da foto no painel reduzido
-		ImageView fotoView = new ImageView(foto);
-		fotoView.setFitHeight(628);
-		fotoView.setFitWidth(1000);
-		fotoView.fitHeightProperty();
-		fotoView.fitWidthProperty();
-
 		GridPane gridPane = new GridPane();
-		gridPane.add(fotoView, 8, 4, 8, 8);
-		gridPane.add(tblClientes, 10, 9, 3, 2);
-		gridPane.setHgap(15);
-		gridPane.setVgap(15);
+		gridPane.add(tblClientes, 0, 0, 1, 1);
+		gridPane.setAlignment(Pos.CENTER);
 		root.setCenter(gridPane);
 	}
 
@@ -221,10 +205,117 @@ public class FrmPrincipal implements Initializable{
 	}
 
 	/*
-	 * Painel vazio
+	 * Painel com imagens de nutrição
 	 */
-	private void montaPainelVazio() {
+	private void montaPainelImagens() {
+		Image food01 = new Image(MainClass.caminho() + "/food01.png");
+		ImageView ivFood01 = new ImageView(food01);
+		Image food02 = new Image(MainClass.caminho() + "/food02.png");
+		ImageView ivFood02 = new ImageView(food02);
+		Image food03 = new Image(MainClass.caminho() + "/food03.png");
+		ImageView ivFood03 = new ImageView(food03);
+		Image food04 = new Image(MainClass.caminho() + "/food04.png");
+		ImageView ivFood04 = new ImageView(food04);
+		Image food05 = new Image(MainClass.caminho() + "/food05.png");
+		ImageView ivFood05 = new ImageView(food05);
+		Image food06 = new Image(MainClass.caminho() + "/food06.png");
+		ImageView ivFood06 = new ImageView(food06);
+		Image food07 = new Image(MainClass.caminho() + "/food07.png");
+		ImageView ivFood07 = new ImageView(food07);
+		Image food08 = new Image(MainClass.caminho() + "/food08.png");
+		ImageView ivFood08 = new ImageView(food08);
+		Image food09 = new Image(MainClass.caminho() + "/food09.png");
+		ImageView ivFood09 = new ImageView(food09);
+		Image food10 = new Image(MainClass.caminho() + "/food10.png");
+		ImageView ivFood10 = new ImageView(food10);
+		Image food11 = new Image(MainClass.caminho() + "/food11.png");
+		ImageView ivFood11 = new ImageView(food11);
+		Image food12 = new Image(MainClass.caminho() + "/food12.png");
+		ImageView ivFood12 = new ImageView(food12);
+		Image food13 = new Image(MainClass.caminho() + "/food13.png");
+		ImageView ivFood13 = new ImageView(food13);
+		Image food14 = new Image(MainClass.caminho() + "/food14.png");
+		ImageView ivFood14 = new ImageView(food14);
+		Image food15 = new Image(MainClass.caminho() + "/food15.png");
+		ImageView ivFood15 = new ImageView(food15);
+		Image food16 = new Image(MainClass.caminho() + "/food16.png");
+		ImageView ivFood16 = new ImageView(food16);
+		Image food17 = new Image(MainClass.caminho() + "/food17.png");
+		ImageView ivFood17 = new ImageView(food17);
+		Image food18 = new Image(MainClass.caminho() + "/food18.png");
+		ImageView ivFood18 = new ImageView(food18);
+		Image food19 = new Image(MainClass.caminho() + "/food19.png");
+		ImageView ivFood19 = new ImageView(food19);
+		Image food20 = new Image(MainClass.caminho() + "/food20.png");
+		ImageView ivFood20 = new ImageView(food20);
+		Image food21 = new Image(MainClass.caminho() + "/food21.png");
+		ImageView ivFood21 = new ImageView(food21);
+		Image food22 = new Image(MainClass.caminho() + "/food22.png");
+		ImageView ivFood22 = new ImageView(food22);
+		Image food23 = new Image(MainClass.caminho() + "/food23.png");
+		ImageView ivFood23 = new ImageView(food23);
+		Image food24 = new Image(MainClass.caminho() + "/food24.png");
+		ImageView ivFood24 = new ImageView(food24);
+		Image food25 = new Image(MainClass.caminho() + "/food25.png");
+		ImageView ivFood25 = new ImageView(food25);
+		Image food26 = new Image(MainClass.caminho() + "/food26.png");
+		ImageView ivFood26 = new ImageView(food26);
+		Image food27 = new Image(MainClass.caminho() + "/food27.png");
+		ImageView ivFood27 = new ImageView(food27);
+		Image food28 = new Image(MainClass.caminho() + "/food28.png");
+		ImageView ivFood28 = new ImageView(food28);
+		Image food29 = new Image(MainClass.caminho() + "/food29.png");
+		ImageView ivFood29 = new ImageView(food29);
+		Image food30 = new Image(MainClass.caminho() + "/food30.png");
+		ImageView ivFood30 = new ImageView(food30);
+		Image food31 = new Image(MainClass.caminho() + "/food31.png");
+		ImageView ivFood31 = new ImageView(food31);
+		Image food32 = new Image(MainClass.caminho() + "/food32.png");
+		ImageView ivFood32 = new ImageView(food32);
+		Image food33 = new Image(MainClass.caminho() + "/food33.png");
+		ImageView ivFood33 = new ImageView(food33);
+		Image food34 = new Image(MainClass.caminho() + "/food34.png");
+		ImageView ivFood34 = new ImageView(food34);
+		Image food35 = new Image(MainClass.caminho() + "/food35.png");
+		ImageView ivFood35 = new ImageView(food35);
 		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.add(ivFood01, 2, 0);
+		grid.add(ivFood02, 3, 0);
+		grid.add(ivFood03, 4, 0);
+		grid.add(ivFood04, 5, 0);
+		grid.add(ivFood05, 6, 0);
+		grid.add(ivFood06, 7, 0);
+		grid.add(ivFood07, 8, 0);
+		grid.add(ivFood08, 2, 1);
+		grid.add(ivFood09, 3, 1);
+		grid.add(ivFood10, 4, 1);
+		grid.add(ivFood11, 5, 1);
+		grid.add(ivFood12, 6, 1);
+		grid.add(ivFood13, 7, 1);
+		grid.add(ivFood14, 8, 1);
+		grid.add(ivFood15, 2, 2);
+		grid.add(ivFood16, 3, 2);
+		grid.add(ivFood17, 4, 2);
+		grid.add(ivFood18, 5, 2);
+		grid.add(ivFood19, 6, 2);
+		grid.add(ivFood20, 7, 2);
+		grid.add(ivFood21, 8, 2);
+		grid.add(ivFood22, 2, 3);
+		grid.add(ivFood23, 3, 3);
+		grid.add(ivFood24, 4, 3);
+		grid.add(ivFood25, 5, 3);
+		grid.add(ivFood26, 6, 3);
+		grid.add(ivFood27, 7, 3);
+		grid.add(ivFood28, 8, 3);
+		grid.add(ivFood29, 2, 4);
+		grid.add(ivFood30, 3, 4);
+		grid.add(ivFood31, 4, 4);
+		grid.add(ivFood32, 5, 4);
+		grid.add(ivFood33, 6, 4);
+		grid.add(ivFood34, 7, 4);
+		grid.add(ivFood35, 8, 4);
 		root.setCenter(grid);
 	}
 
@@ -268,7 +359,7 @@ public class FrmPrincipal implements Initializable{
 				break;
 			}
 			case 4:{
-				montaPainelVazio();
+				montaPainelImagens();
 				break;
 			}
 			case 5: {
